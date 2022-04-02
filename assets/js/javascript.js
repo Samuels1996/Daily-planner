a6b337ef50dbf985edd50d15aede44f182622b4c
brian samuels <samuels.brian9@gmail.com>
2022-04-01 20:05:35 -0400

    adding in additonal files and styling

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,5 +1,10 @@
 var listItem = document.querySelector('container')
 var timeEl = document.getElementById('timeSlot')
 
-var today = moment();
-$('currentDay').text(today.format("MMM Do, YYYY"));
+$(document).ready(function() {
+    var update = function() {
+        document.getElementById('currentDay')
+        .innerHTML = moment().format('dddd, MMMM Do YYYY, h:mm:ss a');
+    }
+    setInterval(update, 1000)
+})
